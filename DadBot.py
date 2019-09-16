8b99e8a3a00e7aefca4cdb7c688b74fe02fb687f
Anton_Hirschl <programstarter@gmail.com>
2019-09-16 12:03:11 -0700

    added opt in out with nick reset

--- DadBot.py
+++ DadBot.py
@@ -9,9 +9,13 @@ bot = discord.Client()
 
 names = ["somthing went wrong"]
 
+optIN = {}
+optOut = {}
+
 @bot.event
 async def on_ready():
     global names
+    global optIN
     print('Bot ready!')
     print(bot.user.name)
     print('-------')
@@ -19,6 +23,16 @@ async def on_ready():
         names = pickle.load(open("names.p", "rb"))
     else:
         names = ["DadBot"]*20
+
+    if os.path.exists('optIN.p'):
+        optIN = pickle.load(open("optIN.p", "rb"))
+    else:
+        optIN = {}
+
+    if os.path.exists('optOut.p'):
+        optOut = pickle.load(open("optOut.p", "rb"))
+    else:
+        optOut = {}
 
 
 @bot.event
@@ -29,8 +43,31 @@ async def on_guild_join(guild):
 @bot.event
 async def on_message(message):
     global names
+    global optIN
+    global optOut
     try:
         if message.author == message.guild.me:
+            return
+
+        if lower(message.content) == "optin":
+            if not optIN.get(message.author.id, False):
+                if not message.guild.me.top_role.permissions.manage_nicknames:
+                    await message.channel.send("I cannot change nicknames, but I'll put you on the list.")
+                elif message.author.top_role > message.guild.me.top_role:
+                    await message.channel.send("You outrank me, but I'll put you on the list.")
+                optIN[message.author.id] = True
+                pickle.dump(optIN, open("optIN.p", "wb"))
+
+                optOut[message.author.id] = message.author.display_name
+                pickle.dump(optOut, open("optOut.p", "wb"))
+            return
+
+        if lower(message.content) == "optout":
+            if optIN.get(message.author.id, False):
+                optIN.pop(message.author.id)
+                pickle.dump(optIN, open("optIN.p", "wb"))
+                if optIN[message.author.id] != message.author.display_name:
+                    await message.author.edit(nick=optOut[message.author.id], reason="DadBot")
             return
 
         word = re.search(r'\bi\'?m\s+(.*)', message.content, re.IGNORECASE)
@@ -39,32 +76,38 @@ async def on_message(message):
         if len(word.group(1)) > 32:
             word = re.search(r'\bi\'?m\s+(\w+)', message.content, re.IGNORECASE)
         word = word.group(1)
-        if len(word) > 32:
-            word = '<LONG DADJOKE>'
+
+        if lower(word) == lower("DadBot"):
+            await message.channel.send("Wait, you're me?")
+            return
+
+        if lower(word) == lower("Dad"):
+            await message.channel.send("Wait, you're me?")
+            return
 
         shuffle(names)
         await message.channel.send("Hi "+word+", I'm " + names.pop() + "!")
         names.append(word)
         pickle.dump(names, open("names.p", "wb"))
 
-        MAYCHANGENAMES = False
-
-        if MAYCHANGENAMES:
+        if optIN.get(message.author.id, False):
             if not message.guild.me.top_role.permissions.manage_nicknames:
-                await message.channel.send(
-                    "Somebody named "+message.guild.owner.display_name+" won't let me change names.")
+                #await message.channel.send(
+                #    "Somebody named "+message.guild.owner.display_name+" won't let me change names.")
                 return
             if message.author.top_role > message.guild.me.top_role:
-                if message.author == message.guild.owner:
-                    await message.channel.send(
-                        "Somebody named "+message.author.display_name+
-                        " won't let me change their name.")
-                else:
-                    await message.channel.send(
-                        "Somebody named "+message.author.display_name+
-                        " won't let me change their name."
-                        " (It might be "+message.guild.owner.display_name+"'s fault)")
+                #if message.author == message.guild.owner:
+                #    await message.channel.send(
+                #        "Somebody named "+message.author.display_name+
+                #        " won't let me change their name.")
+                #else:
+                #    await message.channel.send(
+                #        "Somebody named "+message.author.display_name+
+                #        " won't let me change their name."
+                #        " (It might be "+message.guild.owner.display_name+"'s fault)")
                 return
+            if len(word) > 32:
+                word = word[:27] + "..."
             await message.author.edit(nick=word, reason="DadBot")
 
     except Exception as exc:

90df054d7d08470c2bbd6a036288d9de4c5497bd
Anton_Hirschl <programstarter@gmail.com>
2019-09-16 09:56:08 -0700

    non-nickname changing version

--- DadBot.py
+++ DadBot.py
@@ -1,41 +1,36 @@
 import re
 from random import shuffle
 import discord
+import pickle
+import os
 #permissions 201329664
 
 bot = discord.Client()
 
+names = ["somthing went wrong"]
+
 @bot.event
 async def on_ready():
+    global names
     print('Bot ready!')
     print(bot.user.name)
     print('-------')
+    if os.path.exists('names.p'):
+        names = pickle.load(open("names.p", "rb"))
+    else:
+        names = ["DadBot"]*20
+
 
 @bot.event
 async def on_guild_join(guild):
     await guild.system_channel.send("Hi "+guild.name+", I'm DadBot!")
 
-names = ["DadBot"]*20
 
 @bot.event
 async def on_message(message):
+    global names
     try:
         if message.author == message.guild.me:
-            return
-        if not message.guild.me.top_role.permissions.manage_nicknames:
-            await message.channel.send(
-                "Some twat named "+message.guild.owner.display_name+" won't let me change names.")
-            return
-        if message.author.top_role > message.guild.me.top_role:
-            if message.author == message.guild.owner:
-                await message.channel.send(
-                    "Some twat named "+message.author.display_name+
-                    " won't let me change their name.")
-            else:
-                await message.channel.send(
-                    "Some twat named "+message.author.display_name+
-                    " won't let me change their name."
-                    " (It might be "+message.guild.owner.display_name+"'s fault)")
             return
 
         word = re.search(r'\bi\'?m\s+(.*)', message.content, re.IGNORECASE)
@@ -50,7 +45,27 @@ async def on_message(message):
         shuffle(names)
         await message.channel.send("Hi "+word+", I'm " + names.pop() + "!")
         names.append(word)
-        await message.author.edit(nick=word, reason="DadBot")
+        pickle.dump(names, open("names.p", "wb"))
+
+        MAYCHANGENAMES = False
+
+        if MAYCHANGENAMES:
+            if not message.guild.me.top_role.permissions.manage_nicknames:
+                await message.channel.send(
+                    "Somebody named "+message.guild.owner.display_name+" won't let me change names.")
+                return
+            if message.author.top_role > message.guild.me.top_role:
+                if message.author == message.guild.owner:
+                    await message.channel.send(
+                        "Somebody named "+message.author.display_name+
+                        " won't let me change their name.")
+                else:
+                    await message.channel.send(
+                        "Somebody named "+message.author.display_name+
+                        " won't let me change their name."
+                        " (It might be "+message.guild.owner.display_name+"'s fault)")
+                return
+            await message.author.edit(nick=word, reason="DadBot")
 
     except Exception as exc:
         print(repr(exc))
